2dbcab6ee85ee631f98c563f908b7f183dabd468
rushmi0 <ph.watcharapol_st@tni.ac.th>
2023-12-09 21:54:14 +0700

    update script

--- src/main/resources/script.sql
+++ src/main/resources/script.sql
@@ -11,20 +11,16 @@ CREATE TABLE IF NOT EXISTS UserProfiles
 (
     user_id       SERIAL PRIMARY KEY,
     image_profile VARCHAR(255) DEFAULT 'N/A',
-    username      VARCHAR(255) UNIQUE,                                                    -- ชื่อของผู้ใช้ที่ไม่ซ้ำกัน
+    username      VARCHAR(255) UNIQUE,                                    -- ชื่อของผู้ใช้ที่ไม่ซ้ำกัน
     first_name    VARCHAR(255),
-    last_name     VARCHAR(255),
-    bank_name     VARCHAR(255) DEFAULT 'N/A',
-    bank_number   INTEGER CHECK (LENGTH(CAST(bank_number AS VARCHAR)) = 10) DEFAULT 'N/A',              -- bank_number ต้องมีความยาวที่เท่ากับ 10 ตัวตัวเลข.
+    last_name     VARCHAR(255),                                           -- bank_number ต้องมีความยาวที่เท่ากับ 10 ตัวตัวเลข.
     email         VARCHAR(255) CHECK (email LIKE '%_@_%._%') UNIQUE,
     phone_number  VARCHAR(10) UNIQUE,
     created_at    TIMESTAMPTZ  DEFAULT now(),
-    user_type     UserType,                                                               -- Normal, DogWalkers
-    verification  Verify       DEFAULT 'false' CHECK ( verification IN ('true', 'false')) -- กำหนดค่าเริ่มต้นเป็น false
+    user_type     UserType CHECK (user_type IN ('Normal', 'DogWalkers') ) -- Normal, DogWalkers
 );
 
-SELECT public_key
-FROM userauthentication;
+
 -- //////////////////////////////////////////////////////////////////////////////////////////////////////
 
 -- สร้างตาราง UserAuthentication สำหรับเก็บข้อมูลการยืนยันตัวตนของผู้ใช้
@@ -51,12 +47,14 @@ CREATE INDEX idx_share_key ON UserAuthentication (shared_key);
 -- สร้างตาราง DogWalkers
 CREATE TABLE IF NOT EXISTS DogWalkers
 (
-    walker_id     SERIAL PRIMARY KEY,
-    user_id       INTEGER UNIQUE REFERENCES UserProfiles (user_id), -- ทำการเชื่อมและระบุให้ user_id เป็น unique เพื่อป้องกันความซ้ำซ้อน
-    location_name VARCHAR(255) NOT NULL,
-    price_small   INTEGER      NOT NULL,
-    price_medium  INTEGER      NOT NULL,
-    price_big     INTEGER      NOT NULL
+    walker_id      SERIAL PRIMARY KEY,
+    user_id        INTEGER UNIQUE REFERENCES UserProfiles (user_id),                                                                        -- ทำการเชื่อมและระบุให้ user_id เป็น unique เพื่อป้องกันความซ้ำซ้อน
+    location_name  VARCHAR(255) NOT NULL,
+    id_card_number INTEGER CHECK (LENGTH(CAST(id_card_number AS VARCHAR)) = 10) DEFAULT 'N/A',
+    verification   Verify                                                       DEFAULT 'false' CHECK ( verification IN ('true', 'false')), -- กำหนดค่าเริ่มต้นเป็น false
+    price_small    INTEGER      NOT NULL,
+    price_medium   INTEGER      NOT NULL,
+    price_big      INTEGER      NOT NULL
 );
 
 
@@ -69,7 +67,7 @@ CREATE TABLE IF NOT EXISTS Dogs
     dog_id     SERIAL PRIMARY KEY,
     dog_image  VARCHAR(255) NOT NULL,
     breed_name VARCHAR(255) NOT NULL,
-    size       DogSize      NOT NULL -- small, medium, big
+    size       DogSize      NOT NULL CHECK (size IN ('Small', 'Medium', 'Big') ) -- small, medium, big
 );
 
 -- //////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -81,7 +79,7 @@ CREATE TABLE IF NOT EXISTS DogWalkBookings
     walker_id  INTEGER REFERENCES DogWalkers (walker_id),
     user_id    INTEGER REFERENCES UserProfiles (user_id),
     dog_id     INTEGER REFERENCES Dogs (dog_id),
-    status     State NOT NULL DEFAULT 'Pending',
+    status     State NOT NULL DEFAULT 'Pending' CHECK ( status IN ('Confirm', 'Cancel', 'Pending') ),
     time_start TIME,                        -- เวลาเริ่มจอง
     time_end   TIME,                        -- เวลาสิ้นสุด
     timestamp  TIMESTAMPTZ    DEFAULT now() -- ช่วงเวลาที่ทำการจอง
@@ -109,54 +107,53 @@ CREATE INDEX idx_reviews_userid ON DogWalkerReviews (user_id);
 
 -- //////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
 -- สร้างข้อมูลจำลองในตาราง UserProfiles
-INSERT INTO UserProfiles (image_profile, username, first_name, last_name, email, phone_number, user_type, verification)
-VALUES ('profile1.jpg', 'user1', 'John', 'Doe', 'john.doe@email.com', '1234567890', 'Normal', 'true'),
-       ('profile2.jpg', 'user2', 'Jane', 'Doe', 'jane.doe@email.com', '9876543210', 'DogWalkers', 'false'),
-       ('profile3.jpg', 'user3', 'Bob', 'Smith', 'bob.smith@email.com', '5555555555', 'Normal', 'false'),
-       ('profile4.jpg', 'user4', 'Alice', 'Johnson', 'alice.johnson@email.com', '6666666666', 'DogWalkers', 'true'),
-       ('profile5.jpg', 'user5', 'Charlie', 'Brown', 'charlie.brown@email.com', '7777777777', 'Normal', 'false');
+INSERT INTO UserProfiles (image_profile, username, first_name, last_name, email, phone_number, user_type)
+VALUES ('profile1.jpg', 'user1', 'John', 'Doe', 'john.doe@email.com', '1234567890', 'Normal'),
+       ('profile2.jpg', 'user2', 'Jane', 'Smith', 'jane.smith@email.com', '9876543210', 'DogWalkers'),
+       ('profile3.jpg', 'user3', 'Bob', 'Johnson', 'bob.johnson@email.com', '5555555555', 'Normal'),
+       ('profile4.jpg', 'user4', 'Alice', 'Williams', 'alice.williams@email.com', '1111111111', 'DogWalkers'),
+       ('profile5.jpg', 'user5', 'Charlie', 'Brown', 'charlie.brown@email.com', '9999999999', 'Normal');
 
 -- สร้างข้อมูลจำลองในตาราง UserAuthentication
 INSERT INTO UserAuthentication (user_id, public_key, server_private_key, shared_key)
-VALUES (1, 12345, 67890, 1111),
-       (2, 54321, 98765, 2222),
-       (3, 11111, 99999, 3333),
-       (4, 99999, 11111, 4444),
-       (5, 88888, 22222, 5555);
+VALUES (1, 123456, 789012, 345678),
+       (2, 987654, 321098, 765432),
+       (3, 111222, 333444, 555666),
+       (4, 777888, 999000, 123456),
+       (5, 555111, 222444, 666999);
 
 -- สร้างข้อมูลจำลองในตาราง DogWalkers
-INSERT INTO DogWalkers (user_id, location_name, price_small, price_medium, price_big)
-VALUES (1, 'Park1', 50, 70, 90),
-       (2, 'Park2', 60, 80, 100),
-       (3, 'Park3', 40, 60, 80),
-       (4, 'Park4', 70, 90, 110),
-       (5, 'Park5', 55, 75, 95);
+INSERT INTO DogWalkers (user_id, location_name, id_card_number, verification, price_small, price_medium, price_big)
+VALUES (1, 'Park1', 1234567890, 'true', 50, 70, 90),
+       (2, 'Park2', 0987654321, 'false', 60, 80, 100),
+       (3, 'Park3', 1122334455, 'true', 55, 75, 95),
+       (4, 'Park4', 6677889900, 'false', 65, 85, 105),
+       (5, 'Park5', 5432109876, 'true', 70, 90, 110);
 
 -- สร้างข้อมูลจำลองในตาราง Dogs
 INSERT INTO Dogs (dog_image, breed_name, size)
-VALUES ('dog1.jpg', 'Labrador', 'Big'),
-       ('dog2.jpg', 'Poodle', 'Small'),
-       ('dog3.jpg', 'Beagle', 'Medium'),
-       ('dog4.jpg', 'Husky', 'Big'),
-       ('dog5.jpg', 'Dachshund', 'Small');
+VALUES ('dog1.jpg', 'Golden Retriever', 'Big'),
+       ('dog2.jpg', 'Beagle', 'Small'),
+       ('dog3.jpg', 'Labrador', 'Medium'),
+       ('dog4.jpg', 'Poodle', 'Small'),
+       ('dog5.jpg', 'German Shepherd', 'Big');
 
 -- สร้างข้อมูลจำลองในตาราง DogWalkBookings
 INSERT INTO DogWalkBookings (walker_id, user_id, dog_id, status, time_start, time_end)
-VALUES (1, 3, 2, 'Pending', '10:00', '11:00'),
-       (2, 4, 1, 'Confirm', '14:00', '15:00'),
-       (3, 5, 3, 'Pending', '12:00', '13:00'),
-       (4, 1, 4, 'Cancel', '16:00', '17:00'),
-       (5, 2, 5, 'Confirm', '18:00', '19:00');
+VALUES (1, 2, 1, 'Confirm', '10:00', '11:00'),
+       (2, 3, 2, 'Pending', '12:00', '13:00'),
+       (3, 4, 3, 'Cancel', '14:00', '15:00'),
+       (4, 5, 4, 'Pending', '16:00', '17:00'),
+       (5, 1, 5, 'Confirm', '18:00', '19:00');
 
 -- สร้างข้อมูลจำลองในตาราง DogWalkerReviews
 INSERT INTO DogWalkerReviews (walker_id, user_id, rating, review_text)
-VALUES (1, 3, 4, 'Great walker!'),
-       (2, 4, 5, 'Excellent service!'),
-       (3, 5, 3, 'Average experience'),
-       (4, 1, 2, 'Not satisfied'),
-       (5, 2, 5, 'Highly recommended');
+VALUES (1, 2, 4, 'Great service!'),
+       (2, 3, 5, 'Amazing walker!'),
+       (3, 4, 3, 'Good experience'),
+       (4, 5, 2, 'Not satisfied'),
+       (5, 1, 5, 'Highly recommended');
 
 
 -- //////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -168,19 +165,7 @@ FROM DogWalkBookings;
 SELECT *
 FROM DogWalkers;
 
-SELECT UP.user_id,
-       UP.image_profile,
-       UP.username,
-       UP.first_name,
-       UP.last_name,
-       UP.email,
-       UP.phone_number,
-       UP.created_at,
-       UP.user_type,
-       UP.verification,
-       UA.public_key,
-       UA.server_private_key,
-       UA.shared_key
+SELECT UP.*
 FROM UserProfiles UP
          INNER JOIN
      UserAuthentication UA
